acbb9eb4ee729b9ac2d94a29e4181e53dfa5d7dd
Fábio Akira Yoshida <fabioseno@gmail.com>
2014-03-18 14:02:24 -0300

    Separation of concerns

--- app.js
+++ app.js
@@ -1,20 +1,85 @@
 /*global angular, alert*/
 angular.module('smart', []);
 
-angular.module('smart').controller('smartController', ['$scope', '$q', '$timeout', function ($scope, $q, $timeout) {
+angular.module('smart').factory('pageBuffer',  ['$q', '$timeout', function ($q, $timeout) {
     'use strict';
     
-    var totalPages = 5,
-        pageSize = 5,
-        pagesToBuffer = 1,
-        bufferedPages = {};
+    return function PageBuffer(pageSize, pagesToBuffer) {
+        var bufferedPages = {};
+        
+        return {
+            
+            getData: function getData(currentPage, callback) {
+                var i = 0, startPage = 1, endPage, items = [], defer, promise;
+                
+                defer = $q.defer();
+                
+                startPage = currentPage - pagesToBuffer;
+                endPage = currentPage + pagesToBuffer;
+                
+                if (startPage < 1) {
+                    startPage = 1;
+                }
+                
+                // primeiro serve a página corrente
+                if (!bufferedPages['page' + currentPage]) {
+                    promise = callback(currentPage);
+                    promise.then(function (result) {
+                        bufferedPages['page' + result.currentPage] = result.items;
+                        defer.resolve(result.items);
+                    });
+                } else {
+                    $timeout(function () {
+                        defer.resolve(bufferedPages['page' + currentPage]);
+                    });
+                }
+                
+                // demais páginas
+                for (i = startPage; i <= endPage; i += 1) {
+                    if (i !== currentPage && !bufferedPages['page' + i]) {
+                        promise = callback(i);
+                        promise.then(function (result) {
+                            bufferedPages['page' + result.currentPage] = result.items;
+                        });
+                    }
+                }
+                
+                return defer.promise;
+            }
+        };
+    };
+}]);
+
+
+angular.module('smart').controller('smartController', ['$scope', '$q', '$timeout', 'pageBuffer', 'customDataSource', function ($scope, $q, $timeout, PageBuffer, customDataSource) {
+    'use strict';
     
-    $scope.currentPage = 1;
+    var pageSize = 5,
+        bufferedPages = {},
+        bufferFactory = new PageBuffer(pageSize, 1);
     
     $scope.items = [];
     $scope.logs = [];
     
-    function getLiveData(currentPage, pageSize) {
+    $scope.list = function (currentPage) {
+        bufferFactory.getData(currentPage, function (currentPage) {
+            $scope.logs.push('Retrieving page ' + currentPage + '...');
+            return customDataSource.getLiveData(currentPage, pageSize);
+        }).then(function (result) {
+            $scope.logs.push('Page ' + currentPage + ' ready to be consumed!');
+            
+            $scope.items = result;
+        });
+    };
+    
+    $scope.list($scope.currentPage);
+    
+}]);
+
+angular.module('smart').service('customDataSource', ['$q', '$timeout', function ($q, $timeout) {
+    'use strict';
+    
+    this.getLiveData = function getLiveData(currentPage, pageSize) {
         var items = [], i,
             defer,
             data = {
@@ -23,11 +88,7 @@ angular.module('smart').controller('smartController', ['$scope', '$q', '$timeout
         
         defer = $q.defer();
         
-        $scope.logs.push('Retrieving page ' + currentPage + '...');
-        
         $timeout(function () {
-            $scope.logs.push('Page ' + currentPage + ' ready to be consumed!');
-            
             for (i = 0; i < pageSize; i += 1) {
                 items.push('Item ' + ((currentPage - 1) * pageSize + 1 + i));
             }
@@ -38,57 +99,5 @@ angular.module('smart').controller('smartController', ['$scope', '$q', '$timeout
         }, 2000);
         
         return defer.promise;
-    }
-    
-    function getData(entity, currentPage, pageSize, pagesToBuffer) {
-        var i = 0, startPage = 1, endPage, totalItems = pageSize, items = [],  defer;
-        
-        defer = $q.defer();
-        
-        startPage = currentPage - pagesToBuffer;
-        endPage = currentPage + pagesToBuffer;
-        
-        if (startPage < 1) {
-            startPage = 1;
-        }
-        
-        totalItems = (endPage - startPage + 1) * pageSize;
-        
-        // cria estrutura de armazenamento
-        if (!bufferedPages[entity]) {
-            bufferedPages[entity] = {};
-        }
-        
-        // primeiro serve a página corrente
-        if (!bufferedPages[entity]['page' + currentPage]) {
-            getLiveData(currentPage, pageSize).then(function (result) {
-                bufferedPages[entity]['page' + result.currentPage] = result.items;
-                defer.resolve(result.items);
-            });
-        } else {
-            $timeout(function () {
-                defer.resolve(bufferedPages[entity]['page' + currentPage]);
-            });
-        }
-        
-        // demais páginas
-        for (i = startPage; i <= endPage; i += 1) {
-            if (i !== currentPage && !bufferedPages[entity]['page' + i]) {
-                getLiveData(i, pageSize).then(function (result) {
-                    bufferedPages[entity]['page' + result.currentPage] = result.items; 
-                });
-            }
-        }
-        
-        return defer.promise;
-    }
-    
-    $scope.list = function (currentPage) {
-        getData('users', currentPage, pageSize, pagesToBuffer).then(function (result) {
-            $scope.items = result;
-        });
     };
-    
-    $scope.list($scope.currentPage);
-    
 }]);
